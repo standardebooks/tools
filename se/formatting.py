01e708ded7b1f31f5b35789e225d44bac7b24765
Alex Cabal <alex@alexcabal.com>
2017-06-22 21:58:24 -0500

    Tweak comments

--- se/formatting.py
+++ se/formatting.py
@@ -10,16 +10,16 @@ def make_url_safe(text):
 	#2. Trim
 	text = text.strip()
 
-	#3. Convert title to lowercase.
+	#3. Convert title to lowercase
 	text = text.lower()
 
-	#4. Remove apostrophes.
+	#4. Remove apostrophes
 	text = text.replace("'", "")
 
-	#5. Convert any non-digit, non-letter character to a space.
+	#5. Convert any non-digit, non-letter character to a space
 	text = regex.sub(r"[^0-9a-z]", " ", text, flags=regex.IGNORECASE)
 
-	#6. Convert any instance of one or more space to a dash.
+	#6. Convert any instance of one or more space to a dash
 	text = regex.sub(r"\s+", "-", text)
 
 	#7. Remove trailing dashes
