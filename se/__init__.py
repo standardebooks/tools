b80bb2bc122907662cb6809e34a78003881f362e
Alex Cabal <alex@alexcabal.com>
2019-02-02 14:04:45 -0600

    build: Generalize which selectors to simplify, and add :only-child

--- se/__init__.py
+++ se/__init__.py
@@ -25,6 +25,7 @@ BINARY_EXTENSIONS = [".jpg", ".jpeg", ".tif", ".tiff", ".bmp", ".png", ".epub", 
 SE_GENRES = ["Adventure", "Autobiography", "Biography", "Childrens", "Comedy", "Drama", "Fantasy", "Fiction", "Horror", "Memoir", "Mystery", "Nonfiction", "Philosophy", "Poetry", "Romance", "Satire", "Science Fiction", "Shorts", "Spirituality", "Tragedy", "Travel"]
 ARIA_ROLES = ["afterword", "appendix", "biblioentry", "bibliography", "chapter", "colophon", "conclusion", "dedication", "epigraph", "epilogue", "foreword", "introduction", "noteref", "part", "preface", "prologue", "subtitle", "toc"]
 IGNORED_CLASSES = ["name", "temperature", "state", "era", "compass", "acronym", "postal", "eoc", "initialism", "degree", "time", "compound", "timezone", "signature", "full-page"]
+SELECTORS_TO_SIMPLIFY = [":first-child", ":only-child"]
 MESSAGE_TYPE_WARNING = 1
 MESSAGE_TYPE_ERROR = 2
 COVER_TITLE_BOX_Y = 1620 # In px; note that in SVG, Y starts from the TOP of the image
