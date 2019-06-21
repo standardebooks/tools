2c6a02a4d9eeac424bcd6ba2cfd9fdc3e9f9f474
Alex Cabal <alex@alexcabal.com>
2019-06-21 17:39:53 -0500

    modernize-spelling: Add Timbuctoo -> Timbuktu

--- se/spelling.py
+++ se/spelling.py
@@ -229,6 +229,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\bRoumanian", r"Romanian", xhtml)				# Roumanian -> Romanian
 	xhtml = regex.sub(r"\b([Rr])enascence", r"\1enaissance", xhtml)			# renascence -> renaissance
 	xhtml = regex.sub(r"\bThibet", r"Tibet", xhtml)					# Thibet -> Tibet
+	xhtml = regex.sub(r"\bTimbuctoo", r"Timbuktu", xhtml)				# Timbuctoo -> Timbuktu
 
 	# Remove archaic diphthongs
 	xhtml = regex.sub(r"\b([Mm])edi(æ|ae)val", r"\1edieval", xhtml)
