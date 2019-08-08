2851db253b3e61cb5c3efdbe6f2a15fb1d2539c5
Alex Cabal <alex@alexcabal.com>
2019-08-08 18:16:59 -0500

    modernize-spelling: Add kidnaping -> kidnapping

--- se/spelling.py
+++ se/spelling.py
@@ -211,6 +211,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\bEmpale", r"Impale", xhtml)				# empale -> impale
 	xhtml = regex.sub(r"\bempale", r"impale", xhtml)				# empale -> impale
 	xhtml = regex.sub(r"\b([Tt])abu(s?)\b", r"\1aboo\2", xhtml)			# tabu -> taboo
+	xhtml = regex.sub(r"\b([Kk])idnaping\b", r"\1idnapping", xhtml)			# kidnaping -> kidnapping
 
 
 	# Normalize some names
