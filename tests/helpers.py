0d5d269b92677a49a344ef11f48b699156a9c1d4
Dave Halliday <hallidave@gmail.com>
2019-11-21 18:35:43 -0500

    Initial build test and infrastructure

---
+++ tests/helpers.py
@@ -0,0 +1,25 @@
+import subprocess
+import shlex
+import pytest
+
+def run(cmd: str) -> subprocess.CompletedProcess:
+	"""Run the provided shell string as a command in a subprocess. Returns a
+	status object when the command completes.
+	"""
+	args = shlex.split(cmd)
+	return subprocess.run(args, stderr=subprocess.PIPE, check=False)
+
+def must_run(cmd: str) -> None:
+	"""Run the provided shell string as a command in a subprocess. Forces a
+	test failure if the command fails.
+	"""
+	result = run(cmd)
+	if result.returncode == 0:
+		if not result.stderr:
+			return
+		pytest.fail("stderr was not empty after command '{}'\n{}".format(cmd, result.stderr.decode()))
+	else:
+		fail_msg = "error code {} from command '{}'".format(result.returncode, cmd)
+		if result.stderr:
+			fail_msg += "\n" + result.stderr.decode()
+		pytest.fail(fail_msg)
---
+++ tests/test_build.py
@@ -0,0 +1,26 @@
+import shutil
+from pathlib import Path
+from helpers import must_run
+
+def assemble_book(book_dir: Path, work_dir: Path, data_dir: Path) -> Path:
+	build_dir = work_dir / "book-to-build"
+	# Copy skeleton from book_dir
+	shutil.copytree(book_dir, build_dir)
+	# Add text files for build tests
+	for file in (data_dir / "build" / "text").glob("*.xhtml"):
+		shutil.copy(file, build_dir / "src" / "epub" / "text")
+	# Rebuild file metadata
+	must_run("se print-manifest-and-spine --in-place {}".format(build_dir))
+	must_run("se print-toc --in-place {}".format(build_dir))
+	return build_dir
+
+def test_build_clean(book_dir: Path, work_dir: Path, data_dir: Path, book_name: str):
+	"""Run the build command on a known-clean book and verify that
+	the correct number and type of output files are generated.
+	"""
+	build_dir = assemble_book(book_dir, work_dir, data_dir)
+	must_run("se build --kindle --kobo --check {}".format(build_dir))
+	
+	for suffix in ["epub", "epub3", "azw3", "kepub.epub"]:
+		file = work_dir / (book_name + "." + suffix)
+		assert file.is_file()
