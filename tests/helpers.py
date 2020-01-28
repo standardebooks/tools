33318c5a4f85716fd344dc2467f29aa5e8d6df2c
Dave Halliday <hallidave@gmail.com>
2020-01-27 20:22:09 -0500

    Add tests for print-toc and print-manifest-and-spine

--- tests/helpers.py
+++ tests/helpers.py
@@ -56,14 +56,21 @@ def files_are_golden(in_dir: Path, text_dir: Path, golden_dir: Path, update_gold
 	files = [file.name for file in list(in_dir.glob("*.xhtml"))]
 	assert files
 
+	# Set flag if content.opf is part of test data
+	check_content_opf = (in_dir / "content.opf").is_file()
+	content_opf_out = text_dir.parent / "content.opf"
+
 	# If we want to replace the golden files, copy them before checking.
 	# The checking should always succeed after copying.
 	if update_golden:
 		for file_to_update in files:
 			shutil.copy(text_dir / file_to_update, golden_dir)
+		if check_content_opf:
+			shutil.copy(content_opf_out, golden_dir)
 
 	# Check all files
 	_, mismatches, errors = filecmp.cmpfiles(str(golden_dir), str(text_dir), files)
+
 	# If there are mismatches, do an assert on the text of the files
 	# so that we get a nice context diff from pytest.
 	for mismatch in mismatches:
@@ -73,9 +80,28 @@ def files_are_golden(in_dir: Path, text_dir: Path, golden_dir: Path, update_gold
 			test_text = file.read()
 		assert golden_text == test_text
 
+	if check_content_opf:
+		with open(golden_dir / "content.opf") as file:
+			golden_text = file.read()
+		with open(content_opf_out) as file:
+			test_text = file.read()
+		assert golden_text == test_text
+
 	# Do a redundant check in case there is no text diff for some reason
 	assert mismatches == []
 	# Fail on any other errors
 	assert errors == []
+
+	return True
+
+def output_is_golden(out: str, golden_file: Path, update_golden: bool) ->bool:
+	"""Check that out string matches the contents of the golden file."""
+	if update_golden:
+		with open(golden_file, "w") as file:
+			file.write(out)
+
+	# Output of stdout should match expected output
+	with open(golden_file) as file:
+		assert file.read() == out
 
 	return True
