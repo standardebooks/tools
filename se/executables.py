beeaf91f7232032e250efba26b44e6e4f0c3e9e1
Alex Cabal <alex@alexcabal.com>
2020-02-05 12:55:35 -0600

    se split-files: Add the --filename-format, --start-at, and --template-file options

--- se/executables.py
+++ se/executables.py
@@ -1014,14 +1014,17 @@ def semanticate() -> int:
 
 	return 0
 
-def _split_file_output_file(chapter_number: int, header_xhtml: str, chapter_xhtml: str) -> None:
+def _split_file_output_file(filename_format_string: str, chapter_number: int, template_xhtml: str, chapter_xhtml: str) -> None:
 	"""
 	Helper function for split_file() to write a file given the chapter number,
 	header XHTML, and chapter body XHTML.
 	"""
 
-	with open("chapter-" + str(chapter_number) + ".xhtml", "w", encoding="utf-8") as file:
-		file.write(header_xhtml.replace("NUMBER", str(chapter_number)) + "\n" + chapter_xhtml + "\n</section></body></html>")
+	xhtml = template_xhtml.replace("NUMBER", str(chapter_number))
+	xhtml = xhtml.replace("TEXT", chapter_xhtml)
+
+	with open(filename_format_string.replace("%n", str(chapter_number)), "w", encoding="utf-8") as file:
+		file.write(xhtml)
 		file.truncate()
 
 def split_file() -> int:
@@ -1030,20 +1033,30 @@ def split_file() -> int:
 	"""
 
 	parser = argparse.ArgumentParser(description="Split an XHTML file into many files at all instances of <!--se:split-->, and include a header template for each file.")
+	parser.add_argument("-s", "--start-at", metavar="INTEGER", type=se.is_positive_integer, default="1", help="start numbering chapters at this number, instead of at 1")
+	parser.add_argument("-f", "--filename-format", metavar="STRING", type=str, default="chapter-%%n.xhtml", help="a format string for the output files; `%%n` is replaced with the current chapter number; defaults to `chapter-%%n.xhtml`")
+	parser.add_argument("-t", "--template-file", metavar="FILE", type=str, default="", help="a file containing an XHTML template to use for each chapter; the string NUMBER is replaced by the chapter number, and the string TEXT is replaced by the chapter body")
 	parser.add_argument("filename", metavar="FILE", help="an HTML/XHTML file")
 	args = parser.parse_args()
 
 	try:
 		with open(args.filename, "r", encoding="utf-8") as file:
 			xhtml = se.strip_bom(file.read())
-
 	except FileNotFoundError:
 		se.print_error(f"Not a file: {args.filename}")
+		return se.InvalidFileException.code
 
-	with importlib_resources.open_text("se.data.templates", "header.xhtml", encoding="utf-8") as file:
-		header_xhtml = file.read()
+	if args.template_file:
+		try:
+			with open(args.template_file, "r", encoding="utf-8") as file:
+				template_xhtml = file.read()
+		except FileNotFoundError:
+			se.print_error(f"Not a file: {args.template_file}")
+			return se.InvalidFileException.code
+	else:
+		with importlib_resources.open_text("se.data.templates", "chapter-template.xhtml", encoding="utf-8") as file:
+			template_xhtml = file.read()
 
-	chapter_number = 1
 	chapter_xhtml = ""
 
 	# Remove leading split tags
@@ -1053,16 +1066,16 @@ def split_file() -> int:
 		if "<!--se:split-->" in line:
 			prefix, suffix = line.split("<!--se:split-->")
 			chapter_xhtml = chapter_xhtml + prefix
-			_split_file_output_file(chapter_number, header_xhtml, chapter_xhtml)
+			_split_file_output_file(args.filename_format, args.start_at, template_xhtml, chapter_xhtml)
 
-			chapter_number = chapter_number + 1
+			args.start_at = args.start_at + 1
 			chapter_xhtml = suffix
 
 		else:
 			chapter_xhtml = chapter_xhtml + "\n" + line
 
 	if chapter_xhtml and not chapter_xhtml.isspace():
-		_split_file_output_file(chapter_number, header_xhtml, chapter_xhtml)
+		_split_file_output_file(args.filename_format, args.start_at, template_xhtml, chapter_xhtml)
 
 	return 0
 
