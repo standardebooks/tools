b359e773e05a210cd3f9ca7a01a3dac121337163
Alex Cabal <alex@alexcabal.com>
2020-02-11 17:40:32 -0600

    Rename internal variable for consistency

--- se/executables.py
+++ se/executables.py
@@ -131,7 +131,7 @@ def build() -> int:
 	parser.add_argument("-b", "--kobo", dest="build_kobo", action="store_true", help="also build a .kepub.epub file for Kobo")
 	parser.add_argument("-c", "--check", action="store_true", help="use epubcheck to validate the compatible .epub file; if --kindle is also specified and epubcheck fails, don’t create a Kindle file")
 	parser.add_argument("-k", "--kindle", dest="build_kindle", action="store_true", help="also build an .azw3 file for Kindle")
-	parser.add_argument("-o", "--output-dir", dest="output_directory", metavar="DIRECTORY", type=str, default="", help="a directory to place output files in; will be created if it doesn’t exist")
+	parser.add_argument("-o", "--output-dir", metavar="DIRECTORY", type=str, default="", help="a directory to place output files in; will be created if it doesn’t exist")
 	parser.add_argument("-p", "--proof", action="store_true", help="insert additional CSS rules that are helpful for proofreading; output filenames will end in .proof")
 	parser.add_argument("-t", "--covers", dest="build_covers", action="store_true", help="output the cover and a cover thumbnail; can only be used when there is a single build target")
 	parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
@@ -145,7 +145,7 @@ def build() -> int:
 	for directory in args.directories:
 		try:
 			se_epub = SeEpub(directory)
-			se_epub.build(args.check, args.build_kobo, args.build_kindle, Path(args.output_directory), args.proof, args.build_covers, args.verbose)
+			se_epub.build(args.check, args.build_kobo, args.build_kindle, Path(args.output_dir), args.proof, args.build_covers, args.verbose)
 		except se.SeException as ex:
 			se.print_error(ex, args.verbose)
 			return ex.code
