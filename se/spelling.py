8fa6468f46c3a50b3159ad872860e712c9b71322
Alex Cabal <alex@alexcabal.com>
2020-03-24 20:47:52 -0500

    modernize-spelling: Tchekhov -> Chekhov

--- se/spelling.py
+++ se/spelling.py
@@ -292,6 +292,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\bTimbuctoo", r"Timbuktu", xhtml)				# Timbuctoo -> Timbuktu
 	xhtml = regex.sub(r"\bRumania", r"Romania", xhtml)				# Rumania -> Romania
 	xhtml = regex.sub(r"\bTokio", r"Tokyo", xhtml)					# Tokio -> Tokyo
+	xhtml = regex.sub(r"\bTchekh?ov", r"Chekhov", xhtml)				# Tchekhov -> Chekhov
 
 	# Remove archaic diphthongs
 	xhtml = regex.sub(r"\b([Mm])edi(æ|ae)val", r"\1edieval", xhtml)
