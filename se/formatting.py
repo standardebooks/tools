638238dcabce2713e149af4b247cf0668746339b
maticstric <matic@likar.me>
2020-07-01 08:20:20 +0200

    semanticate: Add abbreviation for gov.

--- se/formatting.py
+++ se/formatting.py
@@ -78,7 +78,7 @@ def semanticate(xhtml: str) -> str:
 	xhtml = regex.sub(r"(?<!\<abbr\>)Inc\.", r"<abbr>Inc.</abbr>", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)Ltd\.", r"<abbr>Ltd.</abbr>", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)St\.", r"<abbr>St.</abbr>", xhtml)
-	xhtml = regex.sub(r"(?<!\<abbr\>)Gov\.", r"<abbr>Gov.</abbr>", xhtml)
+	xhtml = regex.sub(r"(?<!\<abbr\>)[Gg]ov\.", r"<abbr>\1ov.</abbr>", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)MS(S?)\.", r"""<abbr class="initialism">MS\1.</abbr>""", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)([Vv])iz\.", r"<abbr>\1iz.</abbr>", xhtml)
 	xhtml = regex.sub(r"(\b)(?<!\<abbr\>)etc\.", r"\1<abbr>etc.</abbr>", xhtml)
