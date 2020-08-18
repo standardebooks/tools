453d4611ebeb4944568ae4b608052b11d7d66962
Alex Cabal <alex@alexcabal.com>
2020-08-18 12:55:04 -0500

    lint: s-060 should not include pamphlets

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -1558,7 +1558,7 @@ def lint(self, skip_lint_ignore: bool) -> list:
 					messages.append(LintMessage("s-061", "Title and following header content not in a [xhtml]<header>[/] element.", se.MESSAGE_TYPE_ERROR, filename, [node.tostring() for node in nodes]))
 
 				# Check for italics on things that shouldn't be italics
-				nodes = dom.xpath("/html/body//i[contains(@epub:type, 'se:name.music.song') or contains(@epub:type, 'se:name.publication.short-story') or contains(@epub:type, 'se:name.publication.pamphlet') or contains(@epub:type, 'se:name.publication.essay')]")
+				nodes = dom.xpath("/html/body//i[contains(@epub:type, 'se:name.music.song') or contains(@epub:type, 'se:name.publication.short-story') or contains(@epub:type, 'se:name.publication.essay')]")
 				if nodes:
 					messages.append(LintMessage("s-060", "Italics on name that requires quotes instead.", se.MESSAGE_TYPE_WARNING, filename, [node.tostring() for node in nodes]))
 
