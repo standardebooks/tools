339eca8927c0a72c0e82abc13ea0ff3c63578108
Alex Cabal <alex@alexcabal.com>
2020-09-08 12:09:38 -0500

    print-title: If guessin the title, use the first non-namespaced epub:type value; if that doesn't exist, just use the first epub:type value

--- se/formatting.py
+++ se/formatting.py
@@ -1296,8 +1296,18 @@ def generate_title(xhtml) -> str:
 				top_level_wrapper = top_level_wrappers[0]
 
 				# Only guess the title if there is a single value for epub:type
-				if top_level_wrapper.attribute("epub:type") and " " not in top_level_wrapper.attribute("epub:type"):
-					title = titlecase(top_level_wrapper.attribute("epub:type").replace("z3998:", "").replace("-", " "))
+				if top_level_wrapper.attribute("epub:type"):
+					values = top_level_wrapper.attribute("epub:type").split(" ")
+
+					# Get the first non-namespaced value as the title
+					for value in values:
+						if ":" not in value:
+							title = titlecase(value.replace("-", " "))
+							break
+
+					# If there are no non-namespaced values, get the first value as the title
+					if title == "":
+						title = titlecase(values[0].replace("z3998:", "").replace("se:", "").replace("-", " "))
 
 	# Remove odd spaces and word joiners
 	title = regex.sub(fr"[{se.NO_BREAK_SPACE}]", " ", title)
