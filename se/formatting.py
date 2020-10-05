85c2da3e51280082ad9d70376beb6e4fc71cc190
Alex Cabal <alex@alexcabal.com>
2020-10-05 16:10:28 -0500

    titlecase: Add 'Des' and 'De La' to lowercased exceptions

--- se/formatting.py
+++ se/formatting.py
@@ -1058,7 +1058,7 @@ def titlecase(text: str) -> str:
 	text = regex.sub(r"(?:vs\.) The\b", "vs. the", text)
 
 	# Lowercase "de", "von", "van", "le", as in "Charles de Gaulle", "Werner von Braun", etc., and if not the first word and not preceded by an &ldquo;
-	text = regex.sub(r"(?<!^|“)\b(De|Von|Van|Le)\b", lambda result: result.group(1).lower(), text)
+	text = regex.sub(r"(?<!^|“)\b(Von|Van|Le|Des|De La|De)\b", lambda result: result.group(1).lower(), text)
 
 	# Uppercase word following "Or,", since it is probably a subtitle
 	text = regex.sub(r"\bOr, ([\p{Lowercase_Letter}])", lambda result: "Or, " + result.group(1).upper(), text)
