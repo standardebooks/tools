78a24f510069504c4e6ed46be879d524b317ae39
Alex Cabal <alex@alexcabal.com>
2021-04-11 17:05:30 -0500

    build: Replace vh/vw units with percent for compatible epub

--- se/se_epub_build.py
+++ se/se_epub_build.py
@@ -551,6 +551,9 @@ def build(self, run_epubcheck: bool, build_kobo: bool, build_kindle: bool, outpu
 						# `page-break-*: page;` should be come `page-break-*: always;`
 						processed_css = regex.sub(r"(\s+)page-break-(before|after):\s+page;", "\\1page-break-\\2: always;", processed_css)
 
+						# Replace `vw` or `vh` units with percent, a reasonable approximation
+						processed_css = regex.sub(r"([0-9\.]+\s*)v(w|h);", r"\1%;", processed_css)
+
 						if processed_css != css:
 							file.seek(0)
 							file.write(processed_css)
