c4dc0745004caa8987c90e6a7eb4e880d06b14c9
Alex Cabal <alex@alexcabal.com>
2021-05-10 17:40:59 -0500

    lint: Correctly use local-name() in xpath

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -1487,7 +1487,7 @@ def lint(self, skip_lint_ignore: bool) -> list:
 
 				# Check for deprecated MathML elements
 				# Note we dont select directly on element name, because we want to ignore any namespaces that may (or may not) be defined
-				nodes = dom.xpath("/html/body//*[name()='mfenced']")
+				nodes = dom.xpath("/html/body//*[local-name()='mfenced']")
 				if nodes:
 					messages.append(LintMessage("s-017", "[xhtml]<m:mfenced>[/] is deprecated in the MathML spec. Use [xhtml]<m:mrow><m:mo fence=\"true\">(</m:mo>...<m:mo fence=\"true\">)</m:mo></m:mrow>[/].", se.MESSAGE_TYPE_ERROR, filename, {node.to_tag_string() for node in nodes}))
 
@@ -1718,7 +1718,8 @@ def lint(self, skip_lint_ignore: bool) -> list:
 					messages.append(LintMessage("s-062", "[xhtml]<dt>[/] element in a glossary without exactly one [xhtml]<dfn>[/] child.", se.MESSAGE_TYPE_ERROR, filename, [node.to_string() for node in nodes]))
 
 				# Check for empty elements. Elements are empty if they have no children and no non-whitespace text
-				nodes = dom.xpath("/html/body//*[not(self::br) and not(self::hr) and not(self::img) and not(self::td) and not(self::th) and not(self::m:none) and not(self::m:mspace) and not(self::m:mprescripts) and not(self::link)][not(*)][not(normalize-space())]")
+				# Use name() to check for MathML elements, because if the MathML namespace is not declared in a given file and we search by the m: namespace then this xpath will fail entirely.
+				nodes = dom.xpath("/html/body//*[not(self::br) and not(self::hr) and not(self::img) and not(self::td) and not(self::th) and not(self::link) and not(local-name()='none') and not(local-name()='mspace') and not(local-name()='mprescripts')][not(./*)][not(normalize-space())]")
 				if nodes:
 					messages.append(LintMessage("s-010", "Empty element. Use [xhtml]<hr/>[/] for thematic breaks if appropriate.", se.MESSAGE_TYPE_ERROR, filename, [node.to_string() for node in nodes]))
 
