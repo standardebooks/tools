22f479a0a9d4ea790c62269cb95366b16ed66f87
Alex Cabal <alex@alexcabal.com>
2021-05-20 14:38:26 -0400

    semanticate: Improve some new checks

--- se/formatting.py
+++ se/formatting.py
@@ -75,8 +75,8 @@ def semanticate(xhtml: str) -> str:
 	xhtml = regex.sub(r"(?<!\<abbr\>)Mlles\.", r"<abbr>Mlles.</abbr>", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)Messrs\.", r"<abbr>Messrs.</abbr>", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)Messers\.", r"<abbr>Messers.</abbr>", xhtml)
-	xhtml = regex.sub(r"(?<!\<abbr\>)([Vv])ol\.", r"<abbr>\1ol.</abbr>", xhtml)
-	xhtml = regex.sub(r"(?<!\<abbr\>)([Cc])hap\.", r"<abbr>\1hap.</abbr>", xhtml)
+	xhtml = regex.sub(r"\b(?<!\<abbr\>)([Vv])ol\.", r"<abbr>\1ol.</abbr>", xhtml)
+	xhtml = regex.sub(r"\b(?<!\<abbr\>)([Cc])hap\. ([0-9])", r"<abbr>\1hap.</abbr> \2", xhtml) # The number allows us to avoid phrases like `Hello, old chap.`
 	xhtml = regex.sub(r"(?<!\<abbr[^\>]*?\>)(P\.(?:P\.)?S\.(?:S\.)?)", r"""<abbr class="initialism">\1</abbr>""", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)Co\.", r"<abbr>Co.</abbr>", xhtml)
 	xhtml = regex.sub(r"(?<!\<abbr\>)Inc\.", r"<abbr>Inc.</abbr>", xhtml)
