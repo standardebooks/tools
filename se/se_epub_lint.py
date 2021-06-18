c5e4fa4f487054c7051db164130c6a8218fec352
Alex Cabal <alex@alexcabal.com>
2021-06-18 14:50:58 -0500

    lint: Improve t-042

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -1532,8 +1532,8 @@ def lint(self, skip_lint_ignore: bool) -> list:
 				# Check for dashes instead of em-dashes
 				typos = typos + [node.to_string() for node in dom.xpath("/html/body//p[re:test(., '\\s[a-z]+-(the|there|is|and|they|when)\\s')]")]
 
-				# Check for punctuation missing before conjunctions
-				typos = typos + [node.to_string() for node in dom.xpath(f"/html/body//p[re:test(., '\\b[a-z]+\\s(But|And|For|Nor|Yet|Or)\\b[^\\.\\?\\-{se.WORD_JOINER}]')]")]
+				# Check for punctuation missing before conjunctions. Ignore <p> with an <i> child starting in a conjunction, as those are probably book titles or non-English languages
+				typos = typos + [node.to_string() for node in dom.xpath(f"/html/body//p[re:test(., '\\b[a-z]+\\s(But|And|For|Nor|Yet|Or)\\b[^’\\.\\?\\-{se.WORD_JOINER}]') and not(./i[re:test(., '^(But|And|For|Nor|Yet|Or)\\b')])]")]
 
 				if typos:
 					messages.append(LintMessage("t-042", "Possible typo.", se.MESSAGE_TYPE_WARNING, filename, typos))
