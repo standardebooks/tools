48bf5abd62e65746c3ed41b99aaa28b071d77690
Alex Cabal <alex@alexcabal.com>
2021-06-22 13:13:29 -0500

    lint: Improve t-063

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -1769,7 +1769,7 @@ def lint(self, skip_lint_ignore: bool) -> list:
 				# Exclude toto followed by ’ since Toto can be a name.
 				# Exclude <h#> whose entire contents is a matched Latinism as we do not italicize those.
 				# Ignore the ToC because we have different rules there
-				nodes = dom.xpath("/html/body//text()[re:test(., '\\b(a (priori|posteriori|fortiori)|ad (hominem|absurdum|nauseam|infinitum|interim)|in (loco|situ|vitro|absentia|camera)|in toto[^’])\\b', 'i') and not(ancestor-or-self::*[@data-css-font-style='italic']) and not(parent::*[re:test(name(), '^h[1-6]$') and @xml:lang='la']) and not(ancestor::nav[contains(@epub:type, 'toc')]) ]")
+				nodes = dom.xpath("/html/body//text()[re:test(., '\\b(a (priori|posteriori|fortiori)|ad (hominem|absurdum|nauseam|infinitum|interim|valem)|in (loco|situ|vitro|absentia|camera)|in toto[^’])\\b', 'i') and not(ancestor-or-self::*[@data-css-font-style='italic']) and not(parent::*[re:test(name(), '^h[1-6]$') and @xml:lang='la']) and not(ancestor::nav[contains(@epub:type, 'toc')]) ]")
 				if nodes:
 					messages.append(LintMessage("t-063", "Latin phrase set without italics.", se.MESSAGE_TYPE_WARNING, filename, nodes))
 
