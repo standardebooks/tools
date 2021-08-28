df13086b21289b6833713ddff27831d43e802655
Alex Cabal <alex@alexcabal.com>
2021-08-27 19:25:17 -0500

    modernize-spelling: by-the-bye -> by the by

--- se/spelling.py
+++ se/spelling.py
@@ -345,6 +345,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\b([Ll])uny", r"\1oony", xhtml)				# luny -> loony
 	xhtml = regex.sub(r"\b([Vv])icuna", r"\1icuña", xhtml)				# vicuna -> vicuña
 	xhtml = regex.sub(r"\b([Cc])larionet", r"\1larinet", xhtml)			# clarionet -> clarinet
+	xhtml = regex.sub(r"\b([Bb])ye?[\- ]the[\- ]bye?", r"\1y the by", xhtml)	# by-the-bye -> by the by
 
 	# Normalize some names
 	xhtml = regex.sub(r"Moliere", r"Molière", xhtml)				# Moliere -> Molière
