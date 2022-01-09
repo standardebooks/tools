b48bac546a805f393b16c2e872a4d03a7b639630
Alex Cabal <alex@alexcabal.com>
2022-01-09 16:53:47 -0600

    build-manifest: Adjust accessibility metadata if we have images in the manifest

--- se/commands/build_manifest.py
+++ se/commands/build_manifest.py
@@ -4,6 +4,8 @@ This module implements the `se build-manifest` command.
 
 import argparse
 
+from lxml import etree
+
 import se
 import se.easy_xml
 import se.formatting
@@ -38,6 +40,25 @@ def build_manifest(plain_output: bool) -> int:
 				else:
 					for node in se_epub.metadata_dom.xpath("/package"):
 						node.append(se_epub.generate_manifest())
+
+				# If we have images in the manifest, add or remove some accessibility metadata while we're here
+				access_mode_nodes = se_epub.metadata_dom.xpath("/package/metadata/meta[@property='schema:accessMode' and text() = 'visual']")
+				access_mode_sufficient_nodes = se_epub.metadata_dom.xpath("/package/metadata/meta[@property='schema:accessibilityFeature' and text() = 'alternativeText']")
+
+				if se_epub.metadata_dom.xpath("/package/manifest/item[starts-with(@media-type, 'image/') and not(re:test(@href, 'images/(cover\\.svg||logo\\.svg|titlepage\\.svg)$'))]"):
+					# Add access modes if we have images
+					if not access_mode_nodes:
+						se_epub.metadata_dom.xpath("/package/metadata/meta[@property='schema:accessMode' and text() = 'textual']")[0].lxml_element.addnext(etree.XML("<meta property=\"schema:accessMode\">visual</meta>"))
+
+					if not access_mode_sufficient_nodes:
+						se_epub.metadata_dom.xpath("/package/metadata/meta[@property='schema:accessModeSufficient']")[0].lxml_element.addnext(etree.XML("<meta property=\"schema:accessibilityFeature\">alternativeText</meta>"))
+				else:
+					# If we don't have images, then remove any access modes that might be there erroneously
+					for node in access_mode_nodes:
+						node.remove()
+
+					for node in access_mode_sufficient_nodes:
+						node.remove()
 
 				with open(se_epub.metadata_file_path, "w", encoding="utf-8") as file:
 					file.write(se.formatting.format_xml(se_epub.metadata_dom.to_string()))
