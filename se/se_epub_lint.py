7a12e9bc30aaaef71ba5427b200bf9e99ebbc63d
Alex Cabal <alex@alexcabal.com>
2022-02-02 16:06:21 -0600

    lint: Improve t-042

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -1891,6 +1891,7 @@ def lint(self, skip_lint_ignore: bool, allowed_messages: List[str] = None) -> li
 				# Check for missing punctuation in continued quotations
 				# ” said Bob “
 				nodes = dom.xpath(r"/html/body//p[re:test(., '”\s(?:said|[A-Za-z]{2,}ed)\s[A-Za-z]+?(?<!\bthe)(?<!\bto)(?<!\bwith)(?<!\bfrom)(?<!\ba\b)(?<!\bis)\s“') or re:test(., '[^\.]”\s(\bhe\b|\bshe\b|I|[A-Z][a-z]+?)\s(?:said|[A-Za-z]{2,}ed)\s“')]")
+				nodes = nodes + dom.xpath("/html/body//p[re:test(., ',” said [A-Za-z]+? [A-Za-z]+?ly “')]")
 				if nodes:
 					messages.append(LintMessage("t-043", "Dialog tag missing punctuation.", se.MESSAGE_TYPE_WARNING, filename, [node.to_string() for node in nodes]))
 
