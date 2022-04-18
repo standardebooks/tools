2ae779322aa63e25e63a7e4e0f6957bc22fbf5bc
Alex Cabal <alex@alexcabal.com>
2022-04-18 10:19:23 -0500

    recompose-epub: Tweak code style

--- se/se_epub.py
+++ se/se_epub.py
@@ -510,21 +510,22 @@ class SeEpub:
 
 			# Convert background-image URLs (from local.css only) to base64
 			if filename == "local.css":
-				for img in regex.finditer(pattern=r"url\(\"(.+?\.(?:svg|png|jpg))\"\)", string=file_css):
-					img_file = img.captures(1)[0].replace("../", "")
+				for image in regex.finditer(pattern=r"""url\("(.+?\.(?:svg|png|jpg))"\)""", string=file_css):
+					image_path = image.captures(1)[0].replace("../", "")
 
-					with open(self.content_path / img_file, "rb") as binary_file:
+					with open(self.content_path / image_path, "rb") as binary_file:
 						image_contents_base64 = base64.b64encode(binary_file.read()).decode()
 
-					if img_file.endswith(".svg"):
-						replacement = f"url('data:image/svg+xml;base64,{image_contents_base64}')"
-					elif img_file.endswith(".jpg"):
-						replacement = f"url('data:image/jpg;base64,{image_contents_base64}')"
-					elif img_file.endswith(".png"):
-						replacement = f"url('data:image/png;base64,{image_contents_base64}')"
+					if image_path.endswith(".svg"):
+						replacement = f"""url("data:image/svg+xml;base64,{image_contents_base64}")"""
+					elif image_path.endswith(".jpg"):
+						replacement = f"""url("data:image/jpg;base64,{image_contents_base64}")"""
+					elif image_path.endswith(".png"):
+						replacement = f"""url("data:image/png;base64,{image_contents_base64}")"""
 					else:
 						continue
-					file_css = file_css.replace(img.group(0), replacement)
+
+					file_css = file_css.replace(image.group(0), replacement)
 
 			css = css + f"\n\n\n/* {filepath.name} */\n" + file_css
 
