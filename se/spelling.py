3ad925f1c4acb4a36243263e2e5014d0e0243db4
Alex Cabal <alex@alexcabal.com>
2022-10-18 16:12:55 -0500

    modernize-spelling: Various additions

--- se/spelling.py
+++ se/spelling.py
@@ -414,6 +414,9 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\b([Aa])lledg(ing|e[sd])", r"\1lleg\2", xhtml)		# alledge -> allege
 	xhtml = regex.sub(r"\b([Hh])er’s", r"\1ers", xhtml)				# her's -> hers
 	xhtml = regex.sub(r"\b([Pp])igm(y|ies)", r"\1ygm\2", xhtml)			# pigmy -> pygmy
+	xhtml = regex.sub(r"\bencreas", r"increas", xhtml)				# encreas(e|ing) -> increas(e|ing)
+	xhtml = regex.sub(r"\bEncreas", r"Increas", xhtml)				# encreas(e|ing) -> increas(e|ing)
+	xhtml = regex.sub(r"\b([Ee])nterpriz", r"\1nterpris", xhtml)			# enterprize -> enterprise
 
 	# Normalize some names
 	xhtml = regex.sub(r"Moliere", r"Molière", xhtml)				# Moliere -> Molière
