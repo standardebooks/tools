5fdcc0e0d46fb3e3860ec23d2c5d9c8efa89e95c
vr8hub <vr_sebooks@letterboxes.org>
2024-02-25 16:30:01 -0600

    Correct cut-and-paste typo

--- se/commands/shift_illustrations.py
+++ se/commands/shift_illustrations.py
@@ -18,7 +18,7 @@ def shift_illustrations(plain_output: bool) -> int:
 	group.add_argument("-d", "--decrement", action="store_true", help="decrement the target illustration number and all following illustrations")
 	group.add_argument("-i", "--increment", action="store_true", help="increment the target illustration number and all following illustrations")
 	parser.add_argument("-a", "--amount", metavar="NUMBER", dest="amount", default=1, type=se.is_positive_integer, help="the amount to increment or decrement by; defaults to 1")
-	parser.add_argument("target_illustration_number", metavar="ENDNOTE-NUMBER", type=se.is_positive_integer, help="the illustration number to start shifting at")
+	parser.add_argument("target_illustration_number", metavar="ILLUSTRATION-NUMBER", type=se.is_positive_integer, help="the illustration number to start shifting at")
 	parser.add_argument("directory", metavar="DIRECTORY", help="a Standard Ebooks source directory")
 	args = parser.parse_args()
 
