0a073d25bc8a3357cf730d6ee4b77795134f9253
Alex Cabal <alex@alexcabal.com>
2024-03-27 17:18:10 -0500

    modernize-spelling: Fix percent regex

--- se/spelling.py
+++ se/spelling.py
@@ -200,7 +200,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\ba propos\b", r"apropos", xhtml)				# a propos -> apropos
 	xhtml = regex.sub(r"\bper cent(s|ages?)?\b", r"percent\1", xhtml)		# per cent -> percent
 	xhtml = regex.sub(r"\bpercent\.(\s+[\p{Lowercase_Letter}])", r"percent\1", xhtml)		# percent. followed by lowercase -> percent
-	xhtml = regex.sub(r"\bpercent\.[,;:\!\?]", r"percent,", xhtml)			# per cent. -> percent
+	xhtml = regex.sub(r"\bpercent\.([,;:\!\?])", r"percent\1", xhtml)			# per cent. -> percent
 	xhtml = regex.sub(r"\b([Ee])ntree(s?)\b", r"\1ntrée\2", xhtml)			# entree -> entrée
 	xhtml = regex.sub(r"\b([Ff])iance", r"\1iancé", xhtml)				# fiance -> fiancé
 	xhtml = regex.sub(r"\b([Oo])utre\b", r"\1utré", xhtml)				# outre -> outré
