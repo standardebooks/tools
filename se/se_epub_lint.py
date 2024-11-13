670252767f627b15fbbb7f85d79ea7cfd441a078
Alex Cabal <alex@alexcabal.com>
2024-11-13 15:40:33 -0600

    lint: Improve t-076

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -2802,9 +2802,9 @@ def _lint_xhtml_typography_checks(filename: Path, dom: se.easy_xml.EasyXmlTree, 
 		messages.append(LintMessage("t-075", "Word in verse with acute accent for scansion instead of grave accent.", se.MESSAGE_TYPE_WARNING, filename, [node.to_string() for node in filtered_nodes]))
 
 	# Check for graphemes or phonemes that are not italicized.
-	nodes = dom.xpath("/html/body//text()[re:test(., '\\s[a-z]’s\\b')]")
+	nodes = dom.xpath("/html/body//text()[re:test(., '\\s[a-z]’s\\b') and not(contains(., 'p’s and q’s'))]")
 	if nodes:
-		messages.append(LintMessage("t-076", "Grapheme or phoneme not italicized. Hint: Dialect with missing letters should mark missing letters with [text]’[/].", se.MESSAGE_TYPE_ERROR, filename, nodes))
+		messages.append(LintMessage("t-076", "Grapheme or phoneme not italicized. Hint: Dialect with missing letters should mark missing letters with [text]’[/].", se.MESSAGE_TYPE_WARNING, filename, nodes))
 
 	return (messages, missing_files)
 
