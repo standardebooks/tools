751b63777ac25b641ad646f31d075078070b7909
Alex Cabal <alex@alexcabal.com>
2024-11-20 18:18:54 -0600

    modernize-spelling: damn' -> damn

--- se/spelling.py
+++ se/spelling.py
@@ -461,6 +461,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\b([Bb])rief case", r"\1riefcase", xhtml)			# brief case -> briefcase
 	xhtml = regex.sub(r"’([Ff])lu\b", r"\1lu", xhtml)				# 'flu -> flu
 	xhtml = regex.sub(r"\b([Aa])nnexe\b", r"\1nnex", xhtml)				# annexe -> annex
+	xhtml = regex.sub(r"([^\p{Letter}])([Dd])amn’([^\p{Letter}])", r"\1\2amn\3", xhtml)	# damn' -> damn
 
 
 	# Normalize some names
