e48c83cc6cbee27f9ff00d249b34dce3b58d9895
Robin Whittleton <robin@reala.net>
2024-11-24 16:04:24 +0100

    modernize-spelling: lowercase street arab
    We still want to drop the hyphen though. It should also work in the plural.

--- se/spelling.py
+++ se/spelling.py
@@ -304,7 +304,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\b([Hh])iccough", r"\1iccup", xhtml)			# hiccough -> hiccup
 	xhtml = regex.sub(r"\b([Rr])oue(s?)\b", r"\1oué\2", xhtml)			# roue -> roué
 	xhtml = regex.sub(r"\b([Ii])dee fixe\b", r"\1dée fixe\2", xhtml)		# idee fixe -> idée fixe
-	xhtml = regex.sub(r"\b([Ss])treet[\s\-]arab\b", r"\1treet Arab", xhtml)		# street-arab -> street Arab
+	xhtml = regex.sub(r"\b([Ss])treet[\s\-][Aa]rab(s?)\b", r"\1treet arab\2", xhtml)		# street-arab -> street arab
 	xhtml = regex.sub(r"\b[EÉ]migr[eé](?!e)", r"Émigré", xhtml)			# Emigre -> Émigré (but not emigrée, which is French)
 	xhtml = regex.sub(r"\b[eé]migr[eé](?!e)", r"émigré", xhtml)			# emigre -> émigré (but not emigrée, which is French)
 	xhtml = regex.sub(r"\b([Cc])ourtezan", r"\1ourtesan", xhtml)			# courtezan -> courtesan
