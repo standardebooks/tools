feb2f89c6b2514d7e1754d7aadd6112899185b9a
Alex Cabal <alex@alexcabal.com>
2025-01-03 16:14:40 -0600

    modernize-spelling: cliche -> cliché

--- se/spelling.py
+++ se/spelling.py
@@ -468,6 +468,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"Hallowe’en", r"Halloween", xhtml)				# Hallowe’en -> Halloween
 	xhtml = regex.sub(r"’([Pp])lane\b", r"\1lane", xhtml)				# 'plane -> plane
 	xhtml = regex.sub(r"([Bb])ye?-and-([Bb])ye?\b", r"\1y-and-\2y", xhtml)		# by-and-bye, bye-and-by -> by-and-by (Note, may be capitalized in a song title)
+	xhtml = regex.sub(r"\b([Cc])liche([sd]?)\b", r"\1liché\2", xhtml)			# cliche -> cliché
 
 
 	# Normalize some names
