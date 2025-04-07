012612c920e094a5395f1f8826901392cdcc7f68
vr8hub <vr_sebooks@letterboxes.org>
2025-04-07 16:19:51 -0500

    Testing: Update and standardize comments, code to build list of tests

--- tests/test_ebook_commands.py
+++ tests/test_ebook_commands.py
@@ -1,9 +1,9 @@
 """
-Tests for commands that transform epub text files, and require a substantially
-complete ebook directory structure to do so.
-This includes:
-	build-ids, build-images, prepare-release, recompose-epub, renumber-endnotes,
-	shift-endnotes, shift-illustrations
+Tests for commands that transform epub text files and require a substantially
+complete ebook directory structure.
+These include:
+	build-ids, build-images, prepare-release, recompose-epub, renumber-endnotes, shift-endnotes,
+	shift-illustrations
 """
 
 import os
@@ -14,10 +14,10 @@ from helpers import assemble_testbook, must_run, files_are_golden
 module_directory = Path(__file__).parent / "ebook_commands"
 module_tests = []
 
-# the directory can't exist until at least one test exists
+# the module directory should not be created until at least one test exists
 if module_directory.is_dir():
 	# the commands being tested are the list of subdirectories
-	test_commands = [os.path.basename(test_entry.path) for test_entry in os.scandir(module_directory) if test_entry.is_dir()]
+	test_commands = [os.path.basename(s.path) for s in os.scandir(module_directory) if s.is_dir()]
 
 	# each command can have multiple tests, each contained in a separate subdirectory
 	for test_command in test_commands:
@@ -25,6 +25,8 @@ if module_directory.is_dir():
 			if directory_entry.is_dir():
 				module_tests.append([test_command, os.path.basename(directory_entry)])
 	module_tests.sort()
+
+# pass the plain command and subdir name so the test ids are easy to read, e.g. build-ids-test-1
 @pytest.mark.parametrize("command, test", module_tests)
 
 def test_ebook_commands(testbook__directory: Path, work__directory: Path, command: str, test: Path, update_golden: bool):
@@ -36,8 +38,8 @@ def test_ebook_commands(testbook__directory: Path, work__directory: Path, comman
 	# the default command to call is the name of the command directory
 	command_to_use = command
 	test_directory = module_directory / command / test
-	# if a file exists in test_directory with the same name as the {command}-command, e.g. help-command,
-	# each line should contain a full command to use, with any arguments.
+	# if a file exists in test_directory with the name of {command}-command, e.g. build-ids-command,
+	# each line should contain the command to use, with any arguments, e.g. `command --arg1 --arg2`
 	command_file = test_directory / (command + "-command")
 	if command_file.is_file():
 		with open(command_file, "r", encoding="utf-8") as cfile:
@@ -50,9 +52,12 @@ def test_ebook_commands(testbook__directory: Path, work__directory: Path, comman
 	else:
 		commands_to_use = [f"{command} {{book_directory}}"]
 
+	# contains the files specific to the particular test being run
 	in_directory = test_directory / "in"
-	golden_directory = test_directory / "golden"
+	# contains the full ebook structure, with the in_directory files copied over the test ebook files
 	book_directory = assemble_testbook(testbook__directory, in_directory, work__directory)
+	# contains the "golden" files, i.e. the files as they should look after the test
+	golden_directory = test_directory / "golden"
 
 	for command_to_use in commands_to_use:
 		command_to_use = command_to_use.format(book_directory=book_directory)
