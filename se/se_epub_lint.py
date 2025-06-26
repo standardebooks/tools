4335625a243c3c553a311fdeb534beba5c427d7a
Alex Cabal <alex@alexcabal.com>
2025-06-26 11:13:42 -0500

    lint: Add m-066, subject identifiers must be IDs and not URLs

--- se/se_epub_lint.py
+++ se/se_epub_lint.py
@@ -905,6 +905,10 @@ def _lint_metadata_checks(self) -> list:
 	if nodes:
 		messages.append(LintMessage("m-083", "[xhtml]<meta property=\"title-type\">[/] element without sibling element with contents of [val]main[/], [val]subtitle[/], [val]extended[/], or [val]short[/].", se.MESSAGE_TYPE_ERROR, self.metadata_file_path, [node.to_string() for node in nodes]))
 
+	nodes = self.metadata_dom.xpath("/package/metadata/meta[@property='term' and re:test(., '^https?://')]")
+	if nodes:
+		messages.append(LintMessage("m-066", "Subject identifiers must be IDs and not URLs.", se.MESSAGE_TYPE_ERROR, self.metadata_file_path, [node.to_string() for node in nodes]))
+
 	return messages
 
 def _get_malformed_urls(dom: se.easy_xml.EasyXmlTree, filename: Path) -> list:
