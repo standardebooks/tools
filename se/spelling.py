5fb4c9c685f875972bdb010c97e0e93dad2f18bf
Alex Cabal <alex@alexcabal.com>
2025-11-04 11:53:54 -0600

    modernize-spelling: barytone -> baritone

--- se/spelling.py
+++ se/spelling.py
@@ -392,6 +392,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = regex.sub(r"\b[ée]tag[eè]re", r"étagère", xhtml)			# etagere -> étagère
 	xhtml = regex.sub(r"\b([Pp])oignard", r"\1oniard", xhtml)			# poignard -> poniard
 	xhtml = regex.sub(r"\b([Vv])izor", r"\1isor", xhtml)				# vizor -> visor
+	xhtml = regex.sub(r"\b([Bb])arytone", r"\1aritone", xhtml)			# barytone -> baritone
 
         # Remove elision quotes from words that no longer need them.
 	xhtml = regex.sub(r"([^\p{Letter}])’([Bb])us\b", r"\1\2us", xhtml)				# ’bus -> bus
