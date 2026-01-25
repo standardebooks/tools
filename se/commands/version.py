db94ae10ac405a97308e65d7f0950c3ad84c1103
Alex Cabal <alex@alexcabal.com>
2026-01-25 16:18:47 -0600

    Fix mypy error

--- se/commands/version.py
+++ se/commands/version.py
@@ -1,10 +1,10 @@
 """
 This module implements the `se version` command.
 """
+from importlib.metadata import Distribution
+import json
 
 import se
-import json
-from importlib.metadata import Distribution
 
 def version() -> int:
 	"""
@@ -14,7 +14,10 @@ def version() -> int:
 	# This API is discussed in https://github.com/pypa/setuptools/issues/4186. At some point (python 3.12 or 13?), an origin method was added to Distribution, which allows this to be done without needing json, e.g.
 	# 	pkg_is_editable = Distribution.from_name("standardebooks").origin.dir_info.editable
 	direct_url = Distribution.from_name("standardebooks").read_text("direct_url.json")
-	pkg_is_editable = json.loads(direct_url).get("dir_info", {}).get("editable", False)	
+	if direct_url is None:
+		pkg_is_editable = False
+	else:
+		pkg_is_editable = json.loads(direct_url).get("dir_info", {}).get("editable", False)
 
 	se_version = f"{se.VERSION}"
 	if pkg_is_editable:
