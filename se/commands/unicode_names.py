c69fb4ce3ae17127ade441201c7843926b6efeb8
Alex Cabal <alex@alexcabal.com>
2026-06-02 16:19:04 -0500

    Update help text

--- se/commands/unicode_names.py
+++ se/commands/unicode_names.py
@@ -19,7 +19,7 @@ def unicode_names(plain_output: bool) -> int:
 	"""
 
 	parser = argparse.ArgumentParser(description="Display Unicode code points, descriptions, and links to more details for each character in a string. Useful for differentiating between different flavors of spaces, dashes, and invisible characters like word joiners.", prog="[command]se[/] [subcommand]unicode-names[/]", formatter_class=SeHelpFormatter)
-	parser.add_argument("strings", metavar="STRING", nargs="*", help="A Unicode string.")
+	parser.add_argument("strings", metavar="STRING", nargs="+", help="A Unicode string.")
 	args = parser.parse_args()
 
 	console = se.init_console()
