5ee03399a34bcb10c860fd7f0ab64a480e2ba081
Robin Whittleton <robin@reala.net>
2026-06-16 19:20:02 +0200

    modernize-spelling: diarrhœa -> diarrhoea

--- se/spelling.py
+++ se/spelling.py
@@ -587,6 +587,7 @@ def modernize_spelling(xhtml: str) -> str:
 	xhtml = xhtml.replace("Æsthet", r"Aesthet") # Aesthetic, Aesthete, etc.
 	xhtml = xhtml.replace("Cæsar", r"Caesar")
 	xhtml = xhtml.replace("Crœsus", r"Croesus")
+	xhtml = regex.sub(r"\b([Dd])iarrhœa", r"\1iarrhoea", xhtml)
 	xhtml = regex.sub(r"\b([Ff])œtus", r"\1oetus", xhtml)
 	xhtml = regex.sub(r"\b([Hh])æmorrhage", r"\1aemorrhage", xhtml)
 	xhtml = regex.sub(r"\b([Hh])yæna", r"\1yena", xhtml)
